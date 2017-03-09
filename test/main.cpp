ea7422000358cf7390aca17904863fe40c368c3f
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2017-03-09 23:39:19 +0300

    Implemented trivial test

--- test/main.cpp
+++ test/main.cpp
@@ -6,10 +6,31 @@
  */
 
 
-#include <thread-pool.h>
+#include <thread-pool.hpp>
 
 #include <iostream>
 
+template <std::size_t N> void f() {
+	std::cout << "f<" << N << ">()" << "\n";
+}
+
 int main() {
+	ThreadPool threadPool(10);
+	threadPool.addTask(f<1>);
+	threadPool.addTask(f<2>);
+	threadPool.addTask(f<3>);
+	threadPool.addTask(f<4>);
+	threadPool.addTask(f<5>);
+	threadPool.addTask(f<6>);
+	threadPool.addTask(f<7>);
+	threadPool.addTask(f<8>);
+	threadPool.addTask(f<9>);
+	threadPool.addTask(f<10>);
+	threadPool.addTask(f<11>);
+	threadPool.addTask(f<12>);
+	threadPool.addTask(f<13>);
+	threadPool.addTask(f<14>);
+	threadPool.addTask(f<15>);
+	threadPool.addTask(f<16>);
 	return 0;
 }
