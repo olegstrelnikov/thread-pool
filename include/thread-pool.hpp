0529f3a0f6e61df57b0b688c1bc439d4773ec76c
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2017-04-01 03:47:42 +0300

    Use condition_variable instead of sleap

--- include/thread-pool.hpp
+++ include/thread-pool.hpp
@@ -12,10 +12,11 @@
 #include <list>
 #include <thread>
 #include <vector>
+#include <condition_variable>
 
 class ThreadPool {
 public:
-	ThreadPool(std::size_t threadNumber, std::size_t usec = 1) : active_(true), usec_(usec) {
+	ThreadPool(std::size_t threadNumber, std::size_t usec = 1) : active_(true) {
 		threads_.reserve(threadNumber);
 		for (; threadNumber; --threadNumber) {
 			threads_.emplace_back(&ThreadPool::worker_, this);
@@ -23,48 +24,49 @@ public:
 	}
 	typedef void (*task_t)();
 	void addTask(task_t task) {
-		std::lock_guard<std::mutex> guard(m_);
-		tasks_.push_back(task);
-	}
-	~ThreadPool() {
-		while (true) {
-			{
-				std::lock_guard<std::mutex> guard(m_);
-				if (tasks_.empty()) {
-					break;
-				}
-			}
-			std::this_thread::sleep_for(std::chrono::microseconds(usec_));
+		{
+			std::lock_guard<std::mutex> guard(m_);
+			tasks_.push_back(task);
 		}
-		active_ = false;
+		cv_.notify_one();
+	} //addTask()
+	~ThreadPool() {
+		{
+			std::lock_guard<std::mutex> guard(m_);
+			active_ = false;
+		}
+		cv_.notify_all();
 		for (auto& thread: threads_) {
 			thread.join();
 		}
-	}
+	} //~ThreadPool()
 private:
 	void worker_() {
 		task_t task = nullptr;
-		while (active_) {
+		while (true) {
 			{
-				std::lock_guard<std::mutex> guard(m_);
+				std::unique_lock<std::mutex> lock(m_);
 				if (!tasks_.empty()) {
 					task = tasks_.front();
 					tasks_.erase(tasks_.begin());
+				} else if (!active_) {
+					break;
+				} else {
+					cv_.wait(lock);
+					continue;
 				}
 			}
 			if (task) {
 				task();
 				task = nullptr;
-			} else {
-				std::this_thread::sleep_for(std::chrono::microseconds(usec_));
 			}
 		}
 	}
 	bool active_;
-	std::size_t usec_;
 	std::list<task_t> tasks_;
 	std::mutex m_;
 	std::vector<std::thread> threads_;
+	std::condition_variable cv_;
 };
 
 #endif /* INCLUDE_THREAD_POOL_HPP_ */
